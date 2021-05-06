04ca73c2b76d926268a32decb8cecd8ef819c4a7
Kelsey Isaacson <keisaacson@github.com>
2021-05-06 16:35:22 -0500

    Create action to create a discussion

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# create-a-discussion
+# create-a-discussion javascript action
+
+This action creates a GitHub repository discussion from a MD template.
+
+## Inputs
+
+### `category`
+
+**Optional** The discussion categury to create the discussion in. Default `"General"`.
+
+### `filename`
+
+**Optional** The filepath to the markdown template for the discussion. Default `"./.github/DISCUSSION_TEMPLATE.md"`.
+
+### `title`
+
+**Required** The title of the discussion to be created.
+
+### `title-date`
+
+**Optional** Pass `true` to include the date at the end of the discussion title (i.e. `My Discussion for the week of Thursday, May 6th, 2021`). Default is not to include the date.
+
+## Outputs
+
+N/A.
+
+## Example usage
+
+```
+- name: Create discussion
+  id: create_discussion
+  uses: keisaacson/create-a-discussion@v0.0.0
+  env:
+    GITHUB_REPOSITORY: ${{ secrets.GITHUB_REPOSITORY }}
+    DISCUSSIONS_TOKEN: ${{ secrets.GH_DISCUSSIONS_TOKEN }}
+  with:
+    category: "Show and tell"
+    filename: "./.github/DEMO_DISCUSSION_TEMPLATE.md"
+    title: "Demos for the week of "
+    title-date: true
+```
---
+++ index.js
@@ -0,0 +1,97 @@
+const core = require('@actions/core')
+const { Toolkit } = require("actions-toolkit")
+const { graphql } = require("@octokit/graphql")
+const moment = require("moment")
+
+const main = async () => {
+  const authToken = "token " + process.env.DISCUSSIONS_TOKEN
+
+  const repository = await getRepositoryData(authToken)
+  const discussionBody = await readTemplateFile()
+  const discussionTitle = buildDiscussionTitle()
+
+  const categoryName = core.getInput("category") || "General"
+  const category = repository.discussionCategories.nodes.find(c => c.name === categoryName)
+
+  await createDiscussion(authToken, discussionBody, discussionTitle, repository.id, category.id)
+}
+
+const getRepositoryData = async (authToken) => {
+  core.debug(`Querying repository ${process.env.GITHUB_REPOSITORY}`)
+  const repositoryArray = process.env.GITHUB_REPOSITORY.split("/")
+
+  const { repository } = await graphql(
+    `
+      query repositoryId($owner: String!, $name: String!) {
+        repository(owner: $owner, name: $name) {
+          id
+          discussionCategories(first: 10) {
+            nodes {
+              id
+              name
+            }
+          }
+        }
+      }
+    `,
+    {
+      owner: repositoryArray[0],
+      name: repositoryArray[1],
+      headers: {
+        authorization: authToken,
+        "GraphQL-Features": "discussions_api",
+      },
+    }
+  )
+
+  return repository
+}
+
+const readTemplateFile = async () => {
+  const template = core.getInput("filename") || "./.github/DISCUSSION_TEMPLATE.md"
+
+  core.debug(`Reading template file: ${template}`)
+  const tools = new Toolkit()
+  const content = await tools.readFile(template)
+  return content
+}
+
+const buildDiscussionTitle = () => {
+  const titleText = core.getInput("title")
+
+  if (core.getInput("title-date") === "true") {
+    const date = moment().format("dddd, MMMM Do, YYYY")
+    return `${titleText} ${date}`
+  } else {
+    return titleText
+  }
+}
+
+const createDiscussion = async (authToken, body, title, repositoryId, categoryId) => {
+  core.debug(`Creating discussion ${title}`)
+  await graphql(
+    `
+      mutation createDiscussion($input: CreateDiscussionInput!) {
+        createDiscussion(input: $input) {
+          discussion {
+            url
+          }
+        }
+      }
+    `,
+    {
+      input: {
+        body: body,
+        title: title,
+        repositoryId: repositoryId,
+        categoryId: categoryId,
+      },
+      headers: {
+        authorization: authToken,
+        "GraphQL-Features": "discussions_api",
+      },
+    }
+  )
+}
+
+main().catch(err => core.setFailed(err.message))
